cbd865dd5284e51ef31f8e77a0d1c66264a8cdfa
arazhunter <ali_akhavan20@yahoo.com>
2019-04-09 07:36:39 +0430

    modified

---
+++ AliAkhavan/Exercises/exp011.ps1
@@ -0,0 +1,21 @@
+[int] $a = Read-Host "please enter N value"
+[int] $b = Read-Host "please enter N value"
+[int] $c = Read-Host "please enter N value"
+
+if ($a -le $b+$c) {
+    if ($b -le $a+$c) {
+        if ($c -le $a+$b) {
+            Write-Host "You can build a traiangle"
+            
+        }
+        else {
+            write.Read-Host "You cant build a traiangle with these scales"
+        }  
+        } 
+        else {
+            write.Read-Host "You cant build a traiangle with these scales"
+    }    
+}
+else {
+    write.Read-Host "You cant build a traiangle with these scales"
+}
