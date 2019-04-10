bfa8f7c817b35a1c50bcb3a6fc1d99a368f4f055
arazhunter <ali_akhavan20@yahoo.com>
2019-04-10 07:37:23 +0430

    go in good health

---
+++ AliAkhavan/Exercises/exp021.ps1
@@ -0,0 +1,18 @@
+[int] $n = Read-Host "please enter N value"
+[int]$s = 0
+[int]$t = 1
+[int]$p = 1
+[int]$p = $p * $i
+
+            for ($i = 1; $i -le $t  ;$i = $i+1) 
+    {
+        $p = $p * $i
+
+        }
+        for ($t = 1; $i -le $n;$t = $t+1) {
+            $s = $s + $p
+            
+    
+    }
+
+    Write-Host $s 
