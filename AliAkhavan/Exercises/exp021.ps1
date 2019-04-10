a6ef42dad7fe5dab596e3d0792fd41f66a4d85e0
arazhunter <ali_akhavan20@yahoo.com>
2019-04-10 13:49:54 +0430

    modified

--- AliAkhavan/Exercises/exp021.ps1
+++ AliAkhavan/Exercises/exp021.ps1
@@ -2,16 +2,16 @@
 [int]$s = 0
 [int]$t = 1
 
+for ($t = 1; $i -le $n;$t = $t+1) 
+{
+    [int]$p = 1
+    [int]$i = 1
             for ($i = 1; $i -le $t  ;$i = $i+1) 
     {
-        [int]$p = 1
+        
         [int]$p = $p * $i
         $p = $p * $i
 
-        for ($t = 1; $i -le $n;$t = $t+1) {
-            $s = $s + $p
-            
-    
     }
 }
     Write-Host $s
