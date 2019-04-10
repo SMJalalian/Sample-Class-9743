166e92fb756c23be5612ac3df0898eba281192cb
arazhunter <ali_akhavan20@yahoo.com>
2019-04-10 13:08:18 +0430

    done

--- AliAkhavan/Exercises/exp021.ps1
+++ AliAkhavan/Exercises/exp021.ps1
@@ -1,18 +1,17 @@
 [int] $n = Read-Host "please enter N value"
 [int]$s = 0
 [int]$t = 1
-[int]$p = 1
-[int]$p = $p * $i
 
             for ($i = 1; $i -le $t  ;$i = $i+1) 
     {
+        [int]$p = 1
+        [int]$p = $p * $i
         $p = $p * $i
 
-        }
         for ($t = 1; $i -le $n;$t = $t+1) {
             $s = $s + $p
             
     
     }
-
-    Write-Host $s 
+}
+    Write-Host $s
