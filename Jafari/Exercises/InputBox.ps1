ab29183d27314bcfbf4264ca516883233159c121
HJJ <HJJ1367@gmail.com>
2019-04-11 13:24:25 +0430

    Example #20

--- Jafari/Exercises/InputBox.ps1
+++
@@ -1,5 +0,0 @@
-$input = $(
-      Add-Type -AssemblyName Microsoft.VisualBasic
-      [Microsoft.VisualBasic.Interaction]::InputBox('Enter your city','Titlebar Text', 'Default new york')
-     )
-$input
