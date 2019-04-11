9f6c8e87e1d8ac497f5c285517d631f2af9357b3
HJJ <HJJ1367@gmail.com>
2019-04-11 13:08:07 +0430

    Example #18

---
+++ Jafari/Exercises/Ex018.ps1
@@ -0,0 +1,15 @@
+[int] $N = Read-Host "Enter Your Number"
+$Divisors = @()
+$SUM = 0
+for($Counter = 1; $Counter -le $N; $Counter++){
+    if($N % $Counter -eq 0){
+        $Divisors += $Counter
+        $SUM += $Counter
+    }
+}
+Write-Host("Count of $N Divisors Is " + $Divisors.Count + " Where That Are:")
+for ($i = 0; $i -lt $Divisors.Count; $i++) {
+    $Divisors.GetValue($i)
+}
+Write-Host("And The Summation of Them Is: $SUM")
+
