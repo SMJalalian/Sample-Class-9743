5937ba5fd97dc4e632bd8d9fa1a22702841e586f
heydari232y <49425367+heydari232y@users.noreply.github.com>
2019-04-13 09:48:48 +0430

    Delete 4.ps1

--- 4.ps1
+++
@@ -1,9 +0,0 @@
-------4-----
-[int]$a=Read-Host "Sanie kol"
-[int]$h = $a/3600
-Write-Host "Saat" $h
-[int]$b = $a-$h*3600
-[int]$m = $b/60
-Write-Host "Daghighe" $m
-[int]$s = $b-$m*60
-Write-Host "Sanie" $s
