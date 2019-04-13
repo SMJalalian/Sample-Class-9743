cd632aecfedba8b0165e811ecca7817b8ca5334a
heydari232y <49425367+heydari232y@users.noreply.github.com>
2019-04-13 09:31:14 +0430

    Add files via upload

---
+++ 5.ps1
@@ -0,0 +1,6 @@
+---5------
+$i=10
+while ($i -le 98) {
+    Write-Host $i
+    $i=$i+2
+}
---
+++ 9.ps1
@@ -0,0 +1,10 @@
+------9-----
+[int]$n=Read-Host "adad vorodi=" 
+[float]$s=0
+[float]$i=1
+while ($i -le $n) {
+    [float]$s=$s+1/$i
+     $i=$i+2 
+}
+Write-Host "jame"$s
+
