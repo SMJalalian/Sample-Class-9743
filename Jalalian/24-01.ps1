c4392333177494b662d0d769567851a604dda6d4
SMJalalian-Teacher <SM.Jalalian@Outlook.com>
2019-04-13 20:40:22 +0430

    Check2

--- Jalalian/24-01.ps1
+++ Jalalian/24-01.ps1
@@ -27,3 +27,23 @@ foreach ($PC in $PCList) {
 }
 
 #**********************************************
+
+Clear-Host
+function Change-Value {    
+    Write-Host("Variable value in begin of Function: $Test")
+    $Test = 20
+    Write-Host("Variable value befor IF Statement: $Test")
+    if ( $true) {
+        Write-Host("Variable value in begin of IF Statement: $Test")  
+        $Test = 50
+        Write-Host("Variable value in End of IF Statement: $Test")  
+    }
+    $global:Test
+    Write-Host("Variable value in End of Function: $Test")
+}
+
+
+$test = 15
+Write-Host("Variable value befor Function execution : $Test")
+Change-Value
+Write-Host("Variable value After Function execution : $Test")
