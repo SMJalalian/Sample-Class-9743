cd54f3b0b1fcc51524e29d3a2fdea345a14991be
skeghbal <49349412+skeghbal@users.noreply.github.com>
2019-04-15 16:08:05 +0430

    ex026-ex027

---
+++ Karimian/Excercises/ex026.ps1
@@ -0,0 +1,15 @@
+﻿clear-host
+[int]$n = Read-Host("Please enter number")
+$sum=0
+$cntr=0
+while( $n -gt 10 )
+{
+    $sum += $n % 10
+    $n = [math]::Floor( $n / 10 )
+    $cntr++
+}
+$sum += $n
+$cntr ++
+write-Host("majmoo argham is $sum")
+write-Host("tedad argham is $cntr")
+ 
