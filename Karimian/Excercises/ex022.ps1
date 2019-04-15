54dc45054370edc5d3c9be100fcc69f48e8e4f87
skeghbal <49349412+skeghbal@users.noreply.github.com>
2019-04-15 09:20:50 +0430

    ex017..ex025

---
+++ Karimian/Excercises/ex022.ps1
@@ -0,0 +1,12 @@
+﻿clear-host
+for( $j=3 ; $j -lt 500 ; $j++ )
+{
+    write-Host("number of $j is:")
+    for( $i=1 ; $i -le $j/2 ; $i++ )
+    {
+        if ( $j%$i -eq 0 )
+        {
+            $i
+        }    
+    }    
+}
---
+++ Karimian/Excercises/ex024.ps1
@@ -0,0 +1,12 @@
+﻿clear-host
+[int]$n = Read-Host("Please enter number")
+$sum=0
+while($n -le 0)
+{
+    $n = Read-Host("Please try egain")
+}
+for( $i=1 ; $i -le $n ; $i++ )
+{
+    $sum += ($i-1) / $i * [math]::pow(-1,$i)     
+}
+$sum
