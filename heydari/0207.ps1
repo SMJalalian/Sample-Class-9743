ca36c38a640cf47b692d94477921e0e085667db7
mahdi <3422132821@PowerShell.Local>
2019-04-27 20:30:51 +0430

    0207

--- heydari/0207.ps1
+++ heydari/0207.ps1
@@ -1,10 +1,6 @@
-for ( $i = 950000 ; $i -le 952000 ; $i+=100 ) {
-    New-ADOrganizationalUnit -Name $i.ToString() -Path "OU=950000,OU=Domain Objects,DC=PowerShell,DC=Local"
-}
-$OUList = Get-ADOrganizationalUnit -Filter * | Where-Object -Property Name -Like 90*
-foreach ( $OU in $OUList ) {
-    if ( $OU.DistinguishedName.Substring(0,9) -ne "OU=950000") {
-        $OU.DistinguishedName
-    }    
-    
+$list = Import-Csv -Path "C:\Users\3422132821\Repos\Class-9743\heydari\Resources\City.csv"
+foreach ($item in $list){
+$Des = $item.tag + " - " + "شهر " + $item.city
+New-ADOrganizationalUnit -Name $item.code -Path "OU=950000,OU=Domain Objects,DC=PowerShell,DC=Local" -Description $des
+
 }
