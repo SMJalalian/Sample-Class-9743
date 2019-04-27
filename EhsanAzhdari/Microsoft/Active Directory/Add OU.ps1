2fb72033e2098748bc2a83dda997a4168c983cfa
EHSAN-AZHDARI <e_azhdari@outlook.com>
2019-04-27 20:34:24 +0430

    ADD OU

--- EhsanAzhdari/Microsoft/Active Directory/Add OU.ps1
+++ EhsanAzhdari/Microsoft/Active Directory/Add OU.ps1
@@ -4,4 +4,4 @@ $AllCity = Import-Csv -Path "C:\Users\0820146439\Documents\PowerShell\class-9743
 foreach ($City in $AllCity) {
     $Description = $city.TAG + " - " + " شهر " + $city.City
     New-ADOrganizationalUnit -Name $city.Code -Path "OU=930000,OU=Domain Objects,DC=PowerShell,DC=Local" -Description $Description
-}
+} 
