ac2382393289059f77690d056fb3bc91cdba46bc
EHSAN-AZHDARI <e_azhdari@outlook.com>
2019-04-27 20:29:58 +0430

    ADD OU

--- EhsanAzhdari/Microsoft/Active Directory/Add OU.ps1
+++ EhsanAzhdari/Microsoft/Active Directory/Add OU.ps1
@@ -2,6 +2,6 @@ Clear-Host
 $AllCity = Import-Csv -Path "C:\Users\0820146439\Documents\PowerShell\class-9743\EhsanAzhdari\Resources\Final.csv" -Encoding UTF8
 
 foreach ($City in $AllCity) {
-    $Description = $city.TAG + " - " + $city.City + "شهر"
+    $Description = $city.TAG + " - " + " شهر " + $city.City
     New-ADOrganizationalUnit -Name $city.Code -Path "OU=930000,OU=Domain Objects,DC=PowerShell,DC=Local" -Description $Description
 }
