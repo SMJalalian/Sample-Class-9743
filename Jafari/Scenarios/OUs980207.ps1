1f1ba5e9ab020fc959d456e5f66a3fe2f22617a3
HJJ <HJJ@gmail.com>
2019-04-29 20:30:26 +0430

    ComputerAccounts

--- Jafari/Scenarios/OUs980207.ps1
+++ Jafari/Scenarios/OUs980207.ps1
@@ -1,5 +1,5 @@
 New-ADOrganizationalUnit -Name 970000 -Path "OU=Domain Objects,DC=PowerShell,DC=Local" -Description "KER - استان کرمان "
-$Province = import-csv -Path "C:\Projects\class-9743\Jafari\Scenarios\Proviences (2).csv" -Encoding UTF8
+$Province = import-csv -Path "C:\Projects\class-9743\Jafari\Scenarios\Provinces.csv" -Encoding UTF8
 foreach ( $City in $Province ) {
     $Des = $City.Tag + " - " + " شهر " + $City.City
     New-ADOrganizationalUnit -Name $City.Code.ToString() -Path "OU=970000,OU=Domain Objects,DC=PowerShell,DC=Local" -Description $Des
