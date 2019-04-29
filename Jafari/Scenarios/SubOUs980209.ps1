207a5bf8bf911c765caedabe21f8c43d9f50d34d
HJJ <HJJ@gmail.com>
2019-04-29 19:42:54 +0430

    SubOUs

--- Jafari/Scenarios/SubOUs980209.ps1
+++ Jafari/Scenarios/SubOUs980209.ps1
@@ -1,6 +1,7 @@
 $MainOU = Get-ADOrganizationalUnit -Filter * | Where-Object -Property Name -Like 970000
-$Provience = import-csv -Path "C:\Projects\class-9743\Jafari\Scenarios\Proviences (2).csv" -Encoding UTF8
-foreach ( $City in $Provience ) {
+$MainOU
+$Province = import-csv -Path "C:\Projects\class-9743\Jafari\Scenarios\Proviences (2).csv" -Encoding UTF8
+foreach ( $City in $Province ) {
     $DN = "OU=" + $City.Code + "," + $MainOU.DistinguishedName    
     
     New-ADOrganizationalUnit -Name "Computers" -Path $DN
