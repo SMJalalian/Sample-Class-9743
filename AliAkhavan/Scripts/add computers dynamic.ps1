903db59e1d5333373633590dfe60b4bb6adec551
arazhunter <ali_akhavan20@yahoo.com>
2019-05-04 16:26:03 +0430

    new project

---
+++ AliAkhavan/Scripts/add computers dynamic.ps1
@@ -0,0 +1,41 @@
+$City = import-csv -Path "C:\local repos\class-9743\AliAkhavan\Scripts\my town.csv" -Encoding UTF8
+$Dynamicaddress = import-csv -Path "C:\local repos\class-9743\AliAkhavan\Scripts\my town.csv" -Encoding UTF8
+
+
+foreach ($BB in $Dynamicaddress) 
+{
+    
+foreach ( $cc in $City ) {
+$name = "SI" + "-" + $cc.TAG + "-" + "FAV" +  "-"  + 0 + $i.ToString()
+    New-ADComputer -name $name -Description $cc.Code.ToString()
+
+
+    }
+    
+}
+
+for ($i = 1; $i -le 3 ; $i++ ) {
+    
+    foreach ( $cc in $City ) {
+    
+        $name = "SI" + "-" + $cc.TAG + "-" + "MAL" +  "-"  + 0 + $i.ToString()
+            New-ADComputer -name $name -Description $cc.Code.ToString()
+         
+        
+            } 
+            
+            
+        }
+
+        for ($i = 1; $i -le 4 ; $i++ ) {
+    
+            foreach ( $cc in $City ) {
+            
+                $name = "SI" + "-" + $cc.TAG + "-" + "RAV" +  "-"  + 0 + $i.ToString()
+                    New-ADComputer -name $name -Description $cc.Code.ToString()
+                 
+                
+                    } 
+                    
+                     
+                }
