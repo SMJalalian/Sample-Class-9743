98e8f419701ac40e02081fb4e3df45021bfdd552
HJJ <HJJ@gmail.com>
2019-05-04 19:04:15 +0430

    Dynamic Units

---
+++ Jafari/Scenarios/CreatingComputerAccounts-File.ps1
@@ -0,0 +1,4 @@
+$Units = import-csv -Path "C:\Projects\class-9743\Jafari\Scenarios\List.csv" -Encoding UTF8
+$Province = import-csv -Path "C:\Projects\class-9743\Jafari\Scenarios\Provinces.csv" -Encoding UTF8
+foreach ( $City in $Province ) {
+}
