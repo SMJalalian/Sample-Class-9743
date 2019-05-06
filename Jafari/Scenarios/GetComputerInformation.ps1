ca31aa6c9783871e7e56e99dac2c219bcffee761
HJJ <HJJ@gmail.com>
2019-05-06 20:17:38 +0430

    Computer Information

--- Jafari/Scenarios/GetComputerInformation.ps1
+++ Jafari/Scenarios/GetComputerInformation.ps1
@@ -1,10 +1,19 @@
 #$Info = New-Object -TypeName psobject
 #$Info | Add-Member -MemberType NoteProperty -Name RAMSIZE -value (Get-CimInstance CIM_PhysicalMemory).Capacity
-(Get-CimInstance CIM_ComputerSystem).Name
-(Get-CimInstance CIM_OperatingSystem).Caption
-(Get-CimInstance CIM_OperatingSystem).Version
-(Get-CimInstance CIM_OperatingSystem).OSArchitecture
-(Get-CimInstance CIM_DiskDrive).Size/1024/1024/1024
-(Get-CimInstance CIM_ComputerSystem).username
+function Get-PWSystemInfo {
+    param (
+        $PCName
+    )
+    (Get-CimInstance CIM_ComputerSystem -ComputerName $PCName).Name
+    (Get-CimInstance CIM_OperatingSystem -ComputerName $PCName).Caption
+    (Get-CimInstance CIM_OperatingSystem -ComputerName $PCName).Version
+    (Get-CimInstance CIM_OperatingSystem -ComputerName $PCName).OSArchitecture
+    (Get-CimInstance CIM_DiskDrive -ComputerName $PCName).Size/1024/1024/1024
+    (Get-CimInstance CIM_ComputerSystem -ComputerName $PCName).username  
+}
+
+Get-PWSystemInfo server-08
+
+
 
 
