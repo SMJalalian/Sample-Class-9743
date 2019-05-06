a7caffd1d37fe228845646c2b5485ecfcf9d2159
Ali <Ali_akhavan20@yahoo.com>
2019-05-06 20:14:11 +0430

    edsf

--- AliAkhavan/Scripts/ExportInfo.ps1
+++ AliAkhavan/Scripts/ExportInfo.ps1
@@ -1,12 +1,20 @@
-$osinfo = Get-CimInstance Win32_OperatingSystem 
-$RAMinfo = Get-CimInstance Win32_PhysicalMemory
-$hardinfo = Get-CimInstance Win32_DiskDrive
-$computersys = Get-CimInstance Win32_ComputerSystem
+function Get-pwsysteminfo {
+    param (
+        $Pcname
+    )
+    $osinfo      = Get-CimInstance $Pcname Win32_OperatingSystem 
+    $RAMinfo     = Get-CimInstance $Pcname Win32_PhysicalMemory
+    $hardinfo    = Get-CimInstance $Pcname Win32_DiskDrive
+    $computersys = Get-CimInstance $Pcname Win32_ComputerSystem
 
 $osinfo.Caption
 $osinfo.OSType
 $osinfo.Version
+$osinfo.OSArchitecture
 $RAminfo.Capacity
 
 
 $computersys.UserName
+
+}
+
