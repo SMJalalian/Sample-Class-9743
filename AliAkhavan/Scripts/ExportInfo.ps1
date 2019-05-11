81459e6b28637423ab99a6eb551ba355dd10b958
Ali <Ali_akhavan20@yahoo.com>
2019-05-11 19:19:05 +0430

    make an object

--- AliAkhavan/Scripts/ExportInfo.ps1
+++ AliAkhavan/Scripts/ExportInfo.ps1
@@ -8,13 +8,23 @@ function Get-pwsysteminfo {
     $hardinfo    = Get-CimInstance Win32_DiskDrive -ComputerName $Pcname | Measure-Object -Property size -Sum | Foreach {"{0:N2}" -f ([math]::round(($_.Sum / 1GB),2))}
     
 
-$osinfo.Caption
-$osinfo.OSType
-$osinfo.Version
-$osinfo.OSArchitecture
-$totalmemory
-$hardinfo
-$computersys.UserName
+#$osinfo.Caption
+#$osinfo.OSType
+#$osinfo.Version
+#$osinfo.OSArchitecture
+#$totalmemory
+#$hardinfo
+#$computersys.UserName
 
+$obj = new-object psobject
+Add-Member -InputObject $obj -MemberType NoteProperty -Name "oscaption" -Value $osinfo.Caption
+Add-Member -InputObject $obj -MemberType NoteProperty -Name "osversion" -Value $osinfo.Version
+Add-Member -InputObject $obj -MemberType NoteProperty -Name "osArchitecture" -Value $osinfo.OSArchitecture
+Add-Member -InputObject $obj -MemberType NoteProperty -Name "totalmemory" -Value $totalmemory
+Add-Member -InputObject $obj -MemberType NoteProperty -Name "hardinfo" -Value $hardinfo
+Add-Member -InputObject $obj -MemberType NoteProperty -Name "pcusername" -Value $computersys.UserName
+return $obj
 }
-Get-pwsysteminfo -Pcname Server-04
+
+$X = Get-pwsysteminfo -Pcname Server-02
+
