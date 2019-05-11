239fd3d9bd8c200d3e758943c4f389196d20ef50
Jajanyar-Class <b.jahanyar@outlook.com>
2019-05-11 19:33:13 +0430

    12

--- Jahanyar/Systeminfo.ps1
+++ Jahanyar/Systeminfo.ps1
@@ -3,39 +3,41 @@ function Get-Systeminfo {
         $PCName
     )
 
-$os = Get-CimInstance -ComputerName $PCName  Win32_OperatingSystem #| Select-Object osversion, caption, OSArchitecture 
+    $os = Get-CimInstance -ComputerName $PCName  Win32_OperatingSystem #| Select-Object osversion, caption, OSArchitecture 
+    $R = Get-CimInstance -ComputerName $PCName Win32_PhysicalMemory #| Select-Object capacity
 
+    $memory = 0
+    foreach ($item in $R)
+    {
+       $memory += $item.Capacity 
+    }
+    $memory = $memory / (1024*1024)
 
-$R = Get-CimInstance -ComputerName $PCName Win32_PhysicalMemory #| Select-Object capacity
+    $H = Get-CimInstance -ComputerName $PCName win32_diskdrive #| Select-Object size
+    $Hard = 0
+    foreach ($item in $H)
+    {
+        $hard += $item.size
+    }
+    $hard = $hard / (1024*1024*1024)
 
+    $L = Get-CimInstance -ComputerName $PCName win32_computersystem # |Select-Object username
 
-$memory = 0
-foreach ($item in $R)
-{
-    $memory += $item.Capacity 
+    #$os.Name
+    #$os.OSArchitecture
+    #$os.Caption
+    #$os.Version
+    #$memory
+    #$Hard
+    #$L.UserName
+    $info = New-Object psobject
+    $info | Add-Member -MemberType NoteProperty -Name "OSName" -Value $os.CSName
+    $info | Add-Member -MemberType NoteProperty -Name "OSVersio" -Value $os.Version
+    $info | Add-Member -MemberType NoteProperty -Name "OSArch" -Value $os.OSArchitecture
+    $info | Add-Member -MemberType NoteProperty -Name "OSCaption" -Value $os.Caption
+    $info | Add-Member -MemberType NoteProperty -Name "Memory" -Value $memory
+    $info | Add-Member -MemberType NoteProperty -Name "Hard" -Value $Hard
+    $info | Add-Member -MemberType NoteProperty -Name "Logged in user" -Value $L.UserName
+return $info
 }
-$memory = $memory / (1024*1024*1024)
-
-
-
-$H = Get-CimInstance -ComputerName $PCName win32_diskdrive #| Select-Object size
-$Hard = 0
-foreach ($item in $H)
-{
-    $hard += $item.size
-}
-$hard = $hard / (1024*1024*1024)
-
-
-$L = Get-CimInstance -ComputerName $PCName win32_computersystem # |Select-Object username
-
-
-$os.OSArchitecture
-$os.Caption
-$os.Version
-$memory
-$Hard
-$L.UserName
-}
-
-Get-Systeminfo -PCName Server-04
+Get-Systeminfo -PCName Server-02
