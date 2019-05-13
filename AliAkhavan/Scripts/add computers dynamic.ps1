79e7f8714a049d08463c13c7a0356009bb776af4
arazhunter <ali_akhavan20@yahoo.com>
2019-05-13 07:37:57 +0430

    go

--- AliAkhavan/Scripts/add computers dynamic.ps1
+++ AliAkhavan/Scripts/add computers dynamic.ps1
@@ -1,5 +1,5 @@
 $City = import-csv -Path "C:\local repos\class-9743\AliAkhavan\Scripts\my town.csv" -Encoding UTF8
-$Dynamicaddress = import-csv -Path "C:\local repos\class-9743\AliAkhavan\Scripts\finalinfo.csv" -Encoding UTF8
+$Dynamicaddress = import-csv -Path "I:\pws\class-9743\Class-9743-1\AliAkhavan\Scripts\finalinfo.csv" -Encoding UTF8
 
 
 foreach ($info in $Dynamicaddress) 
