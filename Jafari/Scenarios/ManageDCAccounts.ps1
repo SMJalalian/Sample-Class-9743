2e77809b1d4cc17caf1faceca78d7649be46d041
HJJ <HJJ1367@gmail.com>
2019-06-08 06:04:45 +0430

    AD Accounts

---
+++ Jafari/Scenarios/ManageDCAccounts.ps1
@@ -0,0 +1,12 @@
+Clear-Host
+$Today = Get-Date
+[Tim] $Day = Read-Host("Treshold of inactive Days")
+$AllRecords = Search-ADAccount -AccountInactive -TimeSpan $Day | Sort Name
+$DN = "OU=Users,OU=InActive Objects,DC=PowerShell,DC=Local"
+foreach ( $Record in $AllRecords ) {     
+   Disable-ADAccount $Record
+   Move-ADObject $Record $DN
+}
+
+#Get-ADUser -Properties “LastLogonDate”
+
