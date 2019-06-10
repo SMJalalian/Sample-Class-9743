447083d996432d7563a773c6f44d86b5026be4e8
HJJ <HJJ1367@gmail.com>
2019-06-10 06:21:59 +0430

    C#

---
+++ Class9743-Solution/Jafari/Test-Class9743.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management.Automation;
+
+namespace Jafari
+{
+    [Cmdlet(VerbsDiagnostic.Test,"Class9743")]	
+    class Test_Class9743Command : Cmdlet
+    {
+	protected override void BeginProcessing()
+        {
+            WriteObject("This is a test of the BeginProcessing template.");
+        }
+    }
+}
