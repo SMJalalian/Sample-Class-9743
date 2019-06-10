c54ee148cc6ced19aae3b726dbd9875cbc1cbb95
ehsan <ehsan@HOST03>
2019-06-10 19:27:57 +0430

    initial

---
+++ Class9743-Solution/Azhdari/Class-9743.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management.Automation;
+
+namespace Azhdari
+{
+    [Cmdlet(VerbsDiagnostic.Test, "EHSAN")]
+    public class TESTEhsan : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject("This is a TEST For EHSAN");
+        }
+    }
+}
